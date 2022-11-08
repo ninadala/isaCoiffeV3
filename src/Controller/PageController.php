2ef831a66a96ae1c97a9adb1c17c8d3b7d3a88f4
Nina Sellal <nina.sellal@hotmail.fr>
2022-11-08 09:27:50 +0100

    perruquerie page done

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -15,10 +15,19 @@ class PageController extends AbstractController
             
         ]);
     }
+
     #[Route('/onco-coiffure', name: 'onco-coiffure')]
     public function oncoCoiffure(): Response
     {
         return $this->render('page/onco.html.twig', [
+            
+        ]);
+    }
+
+    #[Route('/perruquerie', name: 'perruquerie')]
+    public function perruquerie(): Response
+    {
+        return $this->render('page/perruquerie.html.twig', [
             
         ]);
     }
